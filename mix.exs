2752c19fa34dcb98d451fe7adf7d65bf96aa900f
tomaszpawlak <tomasz.pawlak@andrew-lucas.com>
2018-11-29 15:38:04 +0100

    Remove warnings

--- mix.exs
+++ mix.exs
@@ -8,8 +8,8 @@ defmodule AuthPlug.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env == :prod,
-      description: description,
-      package: package,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
